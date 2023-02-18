e094c9e0113ca64cdf96540cc60ae1b714a65ed4
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-18 14:35:16 +0600

    cahne in count

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
 import Counter from './components/Counter';
 import Stats from './components/Stats';
+import { useState } from 'react';
+
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+
+]
+
 
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0)
+  };
   return (
     <div className="App">
       <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -15,7 +34,7 @@ function App() {
         <div class="max-w-md mx-auto mt-10 space-y-5">
           <Counter></Counter>
           <Counter></Counter>
-          <Stats count={2}></Stats>
+          <Stats count={totalCount()}></Stats>
         </div>
       </div>
 
